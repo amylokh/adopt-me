41f25bc82545463ba962533a4021d04a6aa27b43
amylokh <amylokh@gmail.com>
2021-04-02 10:28:03 +0530

    Add dropdowns for pet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Phoebe" animal="Dog" breed="Labrador" />
-      <Pet name="Doink" animal="Cat" breed="Stray-cat" />
-      <Pet name="MyBird" animal="Bird" breed="Pigeon" />
+      <SearchParams />
     </div>
   );
 };
---
+++ src/SearchParams.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("Dog");
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          <input
+            id="location"
+            value={location}
+            placeholder="location"
+            onChange={(event) => setLocation(event.target.value)}
+          />
+        </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(event) => setAnimal(event.target.value)}
+            onBlur={(event) => setAnimal(event.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
